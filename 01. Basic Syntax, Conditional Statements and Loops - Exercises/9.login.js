47f8837e34ef7da76c9e91ccdfaea7611938bf39
Roman Hristozov <romkata92@gmail.com>
2024-09-20 18:32:32 +0300

    Adding ex 9 and 10

--- 01. Basic Syntax, Conditional Statements and Loops - Exercises/9.login.js
+++ 01. Basic Syntax, Conditional Statements and Loops - Exercises/9.login.js
@@ -1,4 +1,27 @@
-function login() {
+function login(arr) {
+  let username = arr[0];
+  let password = '';
 
+  for (let i = username.length - 1; i >= 0; i--) {
+    let currentChar = username[i];
+    password += currentChar;
+  }
+
+  let currentTry = 1;
+
+  for (let i = 1; i <= arr.length; i++) {
+    let passwordGuess = arr[i];
+    if (passwordGuess === password) {
+      console.log(`User ${username} logged in.`);
+      break;
+    } else {
+      if (currentTry === 4) {
+        console.log(`User ${username} blocked!`);
+        break;
+      }
+      console.log('Incorrect password. Try again.');
+    }
+    currentTry++;
+  }
 }
-login('Acer', 'login', 'go', 'let me in', 'recA');
+login(['Acer', 'login', 'go', 'let me in', 'recA']);
